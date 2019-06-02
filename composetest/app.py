2424d69eef5d2942ae88f02feaf93dd42f9fc4a0
lfccat <48589401+lfccat@users.noreply.github.com>
2019-06-03 01:14:51 +1200

    Update app.py
    finished primeStored funtion

--- composetest/app.py
+++ composetest/app.py
@@ -4,27 +4,19 @@ import redis
 from flask import Flask
 
 app = Flask(__name__)
-cache = redis.Redis(host='redis', port=6379)
+redisClient = redis.Redis(host='redis', port=6379)
+numberList = "numbers"
 
 
-def get_hit_count():
-    retries = 5
-    while True:
-        try:
-            return cache.incr('hits')
-        except redis.exceptions.ConnectionError as exc:
-            if retries == 0:
-                raise exc
-            retries -= 1
-            time.sleep(0.5)
+
+
 
 def prime(num):
     isprime = True
     i = 2
     if num <= 1:
         isprime = False
-    
- 
+        return isprime
     while i <= num:
         if num % i == 0 and i != num:
             isprime = False
@@ -34,22 +26,34 @@ def prime(num):
             i+= 1
     return isprime
 
+@app.route('/primesStored')
+def store():
+    retries = 5
+    count = b''
+    while True:
+       try:
+           for i in range(0, redisClient.llen(numberList)):
+       
+               count += redisClient.lindex(numberList, i)
+               count += b' '
+           return '{}\n'.format(str(count, 'utf-8'))
+       except redis.exceptions.ConnectionError as exc:
+           if retries == 0:
+               raise exc
+           retries -= 1
+           times.sleep(0.5)
+
 
 @app.route('/isPrime/<int:number>')
 def primes(number):
     
     primer = prime(number)
     if primer == True:
+        redisClient.lpush(numberList, number)
+        
+        
         return '{} is prime. \n'.format(number)
     elif primer == False:
         return '{} is not prime. \n'.format(number)
-
-    
-
-
-@app.route('/hello')
-def hello():
-    count = get_hit_count()
-    return 'Hello World! I have been seen {} times.\n'.format(count)
 
 
